0581712c32ee96746a24310b139ddd83c22dd539
tukp09 <smithr-kenzo6473-alu@xtorey.edu.py>
2025-12-18 16:16:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const boton = document.getElementById('boton');
+
+boton.addEventListener('click', () => {
+    window.open(
+      'https://wa.me/595982112319?text=Quiero%20una%20hamburguesa%20por%20favor',
+      '_blank'
+    );
+});
+
+
